5aaafaa3d36299a5f5b5ab72e1744908c7aa116a
Sasindu Nadeeshan <IT22573100@my.sliit.lk>
2024-07-06 20:45:33 +0530

    initial commit

---
+++ app/index.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import {View, ScrollView, SafeAreaView , Text} from "react-native";
+import {Stack, useRouter} from 'expo-router';
+
+import {COLORS, icons, images, SIZES} from '../constants';
+import {Nearbyjobs, Populerjobs, ScreenHeaderBtn,Welcome} from '../components';
+
+const Home = () => {
+    const router = useRouter();
+
+    return (
+        <SafeAreaView style={{flex:1,backgroundColor:COLORS.lightWhite}}>
+            <Stack.Screen
+                options={
+                    {
+                        headerStyle: {backgroundColor: COLORS.lightWhite},
+                        headerShadowVisible: false,
+                        headerLeft: () => (
+                            <ScreenHeaderBtn
+                                iconUrl={icons.menu} dimensions="60%"
+                            />
+                        ),
+                        headerRight: () => (
+                            <ScreenHeaderBtn
+                                iconUrl={icons.profile} dimensions={100}
+                            />
+                        )
+                    }
+                }
+            />
+        </SafeAreaView>
+    );
+}
+
+export default Home;
